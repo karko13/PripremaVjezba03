52d8bb339a62b4d874b3bdfb1047e6c22a8cabba
karko13 <145532713+karko13@users.noreply.github.com>
2023-11-14 18:11:10 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PripremaVjezba03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite neku recenicu ");
+            string x = Console.ReadLine();
+            string[] x1 = x.Split(' ');
+            int b = x1.Length;
+            Console.WriteLine(x1[0]);
+            Console.WriteLine(x1[b-1]);
+            Console.ReadKey();
+
+        }
+    }
+}
